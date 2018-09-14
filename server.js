83c6393fa65236bef367ddbe262def87cd2183d2
brinel321 <brian.c.nelson1@outlook.com>
2018-09-14 16:45:08 -0700

    ready for heroku

--- server.js
+++ server.js
@@ -6,7 +6,7 @@ const express = require('express');
 const PORT = process.env.PORT || 3000;
 const app = express();
 
-const conString = 'postgress://postgres:postgres!@localhost:5432/kilovolt';
+const conString = process.env.DATABASE_URL || 'postgress://postgres:postgres!@localhost:5432/kilovolt';
 const client = new pg.Client(conString);
 client.connect();
 client.on('error', error => {
